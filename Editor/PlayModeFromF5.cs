90578ef078654c2f1f43d14347a6f5e3e4b520e7
baba_s <shotababa1206@gmail.com>
2020-08-20 18:46:38 +0900

    first commit

---
+++ Editor/PlayModeFromF5.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal static class PlayModeFromF5
+	{
+		private const string ITEM_NAME_RUN  = "Edit/UniPlayModeFromF5/Run _F5";
+		private const string ITEM_NAME_STOP = "Edit/UniPlayModeFromF5/Stop #_F5";
+
+		[MenuItem( ITEM_NAME_RUN )]
+		private static void Run()
+		{
+			EditorApplication.isPlaying = true;
+		}
+
+		[MenuItem( ITEM_NAME_RUN, true )]
+		private static bool CanRun()
+		{
+			return !EditorApplication.isPlaying;
+		}
+
+		[MenuItem( ITEM_NAME_STOP )]
+		private static void Stop()
+		{
+			EditorApplication.isPlaying = false;
+		}
+
+		[MenuItem( ITEM_NAME_STOP, true )]
+		private static bool CanStop()
+		{
+			return EditorApplication.isPlaying;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# UniPlayModeFromF5
