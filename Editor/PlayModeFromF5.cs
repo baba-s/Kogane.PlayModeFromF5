95b60beb829b9630ad77cdfe056ac8a7c9ab7557
baba-s <shotababa1206@gmail.com>
2022-08-27 14:41:36 +0900

    update

--- Editor/PlayModeFromF5.cs
+++ Editor/PlayModeFromF5.cs
@@ -1,34 +1,21 @@
 ﻿using UnityEditor;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
 
 namespace Kogane.Internal
 {
-	internal static class PlayModeFromF5
-	{
-		private const string ITEM_NAME_RUN  = "Edit/UniPlayModeFromF5/Run _F5";
-		private const string ITEM_NAME_STOP = "Edit/UniPlayModeFromF5/Stop #_F5";
+    internal static class PlayModeFromF5
+    {
+        [Shortcut( "Kogane/Play", KeyCode.F5 )]
+        private static void Run()
+        {
+            EditorApplication.isPlaying = !EditorApplication.isPlaying;
+        }
 
-		[MenuItem( ITEM_NAME_RUN )]
-		private static void Run()
-		{
-			EditorApplication.isPlaying = true;
-		}
-
-		[MenuItem( ITEM_NAME_RUN, true )]
-		private static bool CanRun()
-		{
-			return !EditorApplication.isPlaying;
-		}
-
-		[MenuItem( ITEM_NAME_STOP )]
-		private static void Stop()
-		{
-			EditorApplication.isPlaying = false;
-		}
-
-		[MenuItem( ITEM_NAME_STOP, true )]
-		private static bool CanStop()
-		{
-			return EditorApplication.isPlaying;
-		}
-	}
+        [Shortcut( "Kogane/Stop", KeyCode.F5, ShortcutModifiers.Shift )]
+        private static void Stop()
+        {
+            EditorApplication.isPlaying = false;
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# UniPlayModeFromF5
+# Kogane Play Mode From F5
 
 F5 で Unity を再生、Shift + F5 で停止できるエディタ拡張
